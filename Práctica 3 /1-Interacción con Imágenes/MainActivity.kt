3c3e40eebb70441df28c099cdf0bbbe5d8f1d335
allisonuq <ausedoq@ulasalle.edu.pe>
2025-03-30 23:59:09 -0500

    Update MainActivity.kt

--- Práctica 3 /1-Interacción con Imágenes/MainActivity.kt
+++ Práctica 3 /1-Interacción con Imágenes/MainActivity.kt
@@ -1,1 +1,28 @@
+// Descripción: Interacción con Imágenes
+// Autor: Allison Mayra Usedo Quispe
+// Fecha creación: 28/03/2025
+
+package com.example.pdm_2025_i
+
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Referencia 
+        val imageView = findViewById<ImageView>(R.id.myImage)
+
+        // Evento de clic
+        imageView.setOnClickListener {
+            Toast.makeText(this, "¡¡¡Has hecho clic en la imagen!!!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
 
