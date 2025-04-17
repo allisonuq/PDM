8892b0ec8f614e83b1ffaa90c304a4db1bc4f431
allisonuq <ausedoq@ulasalle.edu.pe>
2025-04-17 17:05:44 -0500

    Create ResumenActivity.kt
     Creamos  Activity y su layout base.

---
+++ Práctica 4/ Editor de perfil con confirmación/ResumenActivity.kt
@@ -0,0 +1,16 @@
+// ResumenActivity.kt
+// Descripción: Perfil para ingresar y confirmarlo o editarlo.
+// Autor: Allison Mayra Usedo Quispe
+// Fecha creación: 12/04/2025
+
+package com.ejemplo.editorperfil
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResumenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resumen)
+    }
+}
