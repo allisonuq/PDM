a7f43333bd0a14032cbef157f1363e62c220204c
allisonuq <ausedoq@ulasalle.edu.pe>
2025-04-17 16:50:25 -0500

    Create FormularioActivity.kt
    FormularioActivity:
    Implementamos la estructura base con su layout. 
    Esto nos ayuda a establecer la base para el desarrollo del formulario.

---
+++ Práctica 4/ Editor de perfil con confirmación/FormularioActivity.kt
@@ -0,0 +1,18 @@
+// FormularioActivity.kt
+// Descripción: Para que el usuario ingrese  sus datos personales.
+// Autor: Allison Mayra Usedo Quispe
+// Fecha creación: 12/04/2025
+
+package com.ejemplo.editorperfil
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class FormularioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+    }
+}
