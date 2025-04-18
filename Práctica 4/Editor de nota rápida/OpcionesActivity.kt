09a1442c25c91a0efc50e7a09bc5cfe59d1e2025
allisonuq <ausedoq@ulasalle.edu.pe>
2025-04-17 19:34:09 -0500

    Create OpcionesActivity.kt
    Creación de la clase Opciones Activity con estructura base.

---
+++ Práctica 4/Editor de nota rápida/OpcionesActivity.kt
@@ -0,0 +1,16 @@
+// OpcionesActivity.kt
+// Descripción: Muestra la nota y permite compartirla o editarla.
+// Autor: Allison Mayra Usedo Quispe
+// Fecha de creación: 16/04/2025
+
+package com.ejemplo.editornota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OpcionesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_opciones)
+    }
+}
