7bf5d2cfe69fdd70e44d046ce6ef92f140b7ddce
allisonuq <ausedoq@ulasalle.edu.pe>
2025-04-17 19:31:59 -0500

    Rotación de pantalla
    Manejo de rotación de pantalla  para conservar nota

--- Práctica 4/Editor de nota rápida/EditorActivity.kt
+++ Práctica 4/Editor de nota rápida/EditorActivity.kt
@@ -2,6 +2,7 @@
 // Descripción: Pantalla para escribir una nota y enviarla a otra actividad.
 // Autor: Allison Mayra Usedo Quispe
 // Fecha de creación: 16/04/2025
+
 package com.ejemplo.editornota
 
 import android.content.Intent
@@ -15,7 +16,10 @@ class EditorActivity : AppCompatActivity() {
 
     private lateinit var etNota: EditText
 
-    // Registrar para recibir resultado
+    // Clave para guardar estado
+    private val KEY_NOTA = "key_nota"
+
+    // Recibir resultado de Opciones Activity
     private val opcionesLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
         val notaEditada = result.data?.getStringExtra("NOTA_EDITADA")
         if (notaEditada != null) {
@@ -30,6 +34,11 @@ class EditorActivity : AppCompatActivity() {
         etNota = findViewById(R.id.etNota)
         val btnCompartir: Button = findViewById(R.id.btnCompartir)
 
+        // Restaurar  el texto si se gira la pantalla
+        savedInstanceState?.let {
+            etNota.setText(it.getString(KEY_NOTA, ""))
+        }
+
         btnCompartir.setOnClickListener {
             val nota = etNota.text.toString()
             val intent = Intent(this, OpcionesActivity::class.java).apply {
@@ -37,5 +46,11 @@ class EditorActivity : AppCompatActivity() {
             }
             opcionesLauncher.launch(intent)
         }
+    }
+
+    // Guardar el texto al rotar pantalla
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(KEY_NOTA, etNota.text.toString())
     }
 }
