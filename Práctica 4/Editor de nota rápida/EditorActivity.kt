efb702cd89e48cc802c9f9bb193e6abdc3b42d60
allisonuq <ausedoq@ulasalle.edu.pe>
2025-04-17 19:23:06 -0500

    Registro de nota
    Registro de Activity Result Launcher para recibir nota editada

--- Práctica 4/Editor de nota rápida/EditorActivity.kt
+++ Práctica 4/Editor de nota rápida/EditorActivity.kt
@@ -8,27 +8,34 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class EditorActivity : AppCompatActivity() {
 
     private lateinit var etNota: EditText
 
+    // Registrar para recibir resultado
+    private val opcionesLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val notaEditada = result.data?.getStringExtra("NOTA_EDITADA")
+        if (notaEditada != null) {
+            etNota.setText(notaEditada)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_editor)
 
-        // Referencias a vistas
         etNota = findViewById(R.id.etNota)
         val btnCompartir: Button = findViewById(R.id.btnCompartir)
 
-        // Enviar nota a Opciones Activity
         btnCompartir.setOnClickListener {
             val nota = etNota.text.toString()
             val intent = Intent(this, OpcionesActivity::class.java).apply {
                 putExtra("NOTA", nota)
             }
-            startActivity(intent) 
+            opcionesLauncher.launch(intent)
         }
     }
 }
