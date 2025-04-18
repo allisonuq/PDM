e9b8330ba396b180816cd0ff4a31b4f85d4eb1ac
allisonuq <ausedoq@ulasalle.edu.pe>
2025-04-17 19:11:53 -0500

    Create EditorActivity.kt
    Creamos  la clase EditorActivity con estructura base

---
+++ Práctica 4/Editor de nota rápida/EditorActivity.kt
@@ -0,0 +1,16 @@
+// EditorActivity.kt
+// Descripción: Pantalla para escribir una nota y enviarla a otra actividad.
+// Autor: Allison Mayra Usedo Quispe
+// Fecha de creación: 16/04/2025
+
+package com.ejemplo.editornota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editor)
+    }
+}
