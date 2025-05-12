9dfb6e8420e4acdffe4be1def17ef4e43ddcddea
allisonuq <ausedoq@ulasalle.edu.pe>
2025-05-11 23:08:14 -0500

    Estructura base de la pantalla de bienvenida
    Estructura de la pantalla de bienvenida del juego de colores. Incluimos el título "¡¡¡BIENVENIDO!!!" y el subtítulo "Juego de Colores", un diseño con un color de fondo.

---
+++ Examen Parcial/WelcomeScreen.kt
@@ -0,0 +1,40 @@
+package com.example.juegodecolores_examenparcial.screens
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.FontWeight
+
+@Composable
+fun WelcomeScreen() { 
+    Surface(modifier = Modifier.fillMaxSize(), color = Color(0xFFF48FB1)) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(bottom = 60.dp)
+            ) {
+                Text(
+                    text = "¡¡¡BIENVENIDO!!!",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    text = "Juego de Colores",
+                    style = MaterialTheme.typography.headlineMedium
+                )
+            }
+        }
+    }
+}
